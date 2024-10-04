50224090342c4fd1c231564394b74ee517dfd1b2
Vandanakk <143497639+Vandanakk@users.noreply.github.com>
2024-10-04 09:30:36 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+//"use strict" // to treat all js code as newer version.
+//alert("Vandana")
+//alert(3+3) // alert() function is only available when you execute JavaScript in the special context of browser windows. It is available through the window object. Node. js is not intended for writing desktop applications (directly)
+
+/* Datatypes:
+
+number => 2 to power 53
+bigint => for big numbers
+string => ""
+boolean => true/ false
+null => it is a datatype but shown as object when asked about its datatype(it is a javascript bug)
+undefined => the value is not null but not known
+symbol => unique
+object =>
+
+*/
+
+console.log(typeof null)
+console.log(typeof undefined)
