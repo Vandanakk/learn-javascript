b44233b9ee9b620409619ad37ee5e3e308b06eb2
Vandanakk <143497639+Vandanakk@users.noreply.github.com>
2024-10-04 13:53:55 +0000

    container config done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,3 +20,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
 //1 => true and 0 => false
+
+//************************Operations******************************//
+
+let value = 34
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2) // gives respective outcomes
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+// Concatenation
+let str1 = "Vandana"
+let str2 = " Kushwaha"
+let str3 = str1 + str2 // concatenates 2 strings
+console.log(str3) 
+
+console.log("1" + "2") // follow ECMA script guidelines
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+
+// Prefix and postfix increment 
+
+let counter = 100 // read about prefix and postfix increment from js mdn
+counter++
+console.log(counter)
+
+/*
+If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+*/
