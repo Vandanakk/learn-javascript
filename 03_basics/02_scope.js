347f04ac75dd49c8a94b033f76a906bb8439c35c
Vandanakk <143497639+Vandanakk@users.noreply.github.com>
2024-10-12 11:49:43 +0000

    container config done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -14,3 +14,38 @@ if(true){
 
 console.log(c)
 console.log(a);
+
+function one(){
+    const username = "Vandana"
+    function two(){
+        const website = "Netflix"
+        console.log(username)
+    }
+    // console.log(website)             //Can't access website since its in the inner scope.
+    two()                            //After one error, remaining part doesn't get executed.
+}
+
+one()
+
+if(true){
+    const username = "Vandana"
+    if(username == "Vandana"){
+        const website = "Netflix"
+        console.log(username + website)
+    }
+    // console.log(website)                    //Won't get executed bcz its in inner scope
+}
+// console.log(username)                      // Won't get executed
+
+
+function addTwo(num){
+    return num+1
+}
+
+addTwo(4)
+
+const res = function add(num){
+    return num+1
+}
+
+res(6)
