f85527a303bb9c07e45d2d5dc0cdfd6eee9b1a7e
Vandanakk <143497639+Vandanakk@users.noreply.github.com>
2024-10-14 10:19:59 +0000

    container config done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+//Immediately Invoked Function Expressions(IIFE)//
+//Its used because global scope gets poisoned, thus we use IIFE to implement it immediately 
+
+(function one(){
+    console.log("DB Connected")
+})()
+
+( (name) => {
+    console.log(`DB Connected two ${name}`)
+})("Vandana")
